c78c8aa0f43276ac99366bcbf148117b1ad95094
Yasanga32 <yasangashanuka33@gmail.com>
2026-02-21 22:06:58 +0530

    network erro issue fixed

--- server/server.js
+++ server/server.js
@@ -11,23 +11,23 @@ const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : ['http://localhost:5173']
 
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin:allowedOrigins,credentials: true}));
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 
 //API Endpoints
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-app.use('/api/auth',authRouter);
-app.use('/api/user',userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
